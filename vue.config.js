855dddfcc27f3dae6639e247ce83dc479a294c4e
Ben Wo <yewbenwo@gmail.com>
2025-12-23 01:22:46 +0800

    add vite 5 changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// Load .env file manually if needed
+require('dotenv').config()
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
